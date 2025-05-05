0c687790e0def196e13c6e3746b2a5724ea97e54
An <an.sabek01@gmail.com>
2025-05-05 16:31:34 +0300

    added another test for 'hello world'

--- katas/test/test_hello_world.py
+++ katas/test/test_hello_world.py
@@ -5,3 +5,6 @@ from katas.hello_world import hello_world
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "hello world")
+
+    def test_hello_world_negative(self):
+        self.assertNotEqual(hello_world(), "Hello, World!")
