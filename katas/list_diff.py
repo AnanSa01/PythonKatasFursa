a5be1aeb3d2332e08da2d3b800a92cba875b95da
An <an.sabek01@gmail.com>
2025-05-06 10:17:17 +0300

    done with list_diff with 3 unittests

--- katas/list_diff.py
+++ katas/list_diff.py
@@ -8,7 +8,19 @@ def find_difference(numbers):
     Returns:
         the difference between the largest and smallest numbers
     """
-    return 0
+    small = numbers[0]
+    large = numbers[1]
+    if small > large:
+        large = numbers[0]
+        small = numbers[1]
+
+
+    for i in range(2, len(numbers)):
+        if numbers[i] > large:
+            large = numbers[i]
+        elif numbers[i] < small:
+            small = numbers[i]
+    return large - small
 
 
 if __name__ == '__main__':
